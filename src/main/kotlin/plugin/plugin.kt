a8f67a90b742912fd814352c45e5e5d42f2c5d3a
S4ndf1re <jan.kleinmann2000@gmail.com>
2021-03-29 21:15:22 +0200

    Test2

--- src/main/kotlin/plugin/plugin.kt
+++ src/main/kotlin/plugin/plugin.kt
@@ -12,7 +12,7 @@ interface IPlugin {
 class PluginSystem {
     companion object Factory {
         fun create(f: PluginSystem.() -> Unit): PluginSystem {
-            print("Hello")
+            print("Diff")
             val system = PluginSystem()
             system.f()
             return system
