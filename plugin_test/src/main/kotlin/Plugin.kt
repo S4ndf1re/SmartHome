df6cfc4b518f6d0a552b12558e84845baca1df82
S4ndf1re <jan.kleinmann2000@gmail.com>
2021-05-16 15:55:18 +0200

    Added test for TextField and OnOffState Checkbox

--- plugin_test/src/main/kotlin/Plugin.kt
+++ plugin_test/src/main/kotlin/Plugin.kt
@@ -5,6 +5,9 @@ import plugin.implementations.plugin.IPlugin
 import java.nio.charset.StandardCharsets
 
 class Plugin : IPlugin {
+
+    private var state: Boolean = false
+
     override fun init(handler: Mqtt3Client): Boolean {
         handler.toAsync().subscribeWith().topicFilter("test/plugin1").callback {
             val s = StandardCharsets.UTF_8.decode(it.payload.get())
@@ -23,17 +26,57 @@ class Plugin : IPlugin {
         return arrayListOf()
     }
 
-    override fun getContainer(path: String): Container {
-        return Container.create("name") {
-
-            button("btn1") {
-                text = "Click me"
+    override fun getContainers(): List<Container> {
+        return listOf(Container.create("Doorlock1") {
+            button("check1") {
+                text = "Check"
                 onClick = {
-                    println("You clicked me")
+                    println("Check")
                 }
             }
+            button("auth1") {
+                text = "Auth"
+                onClick = {
+                    println("Auth")
+                }
+            }
+        },
+            Container.create("Doorlock2") {
+                button("check2") {
+                    text = "Check"
+                    onClick = {
+                        println("Check")
+                    }
+                }
+                button("auth2") {
+                    text = "Auth"
+                    onClick = {
+                        println("Auth")
+                    }
+                }
+                checkbox("check1") {
+                    text = "Check me"
+                    onOffState = {
+                        println("Off")
+                        state = false
+                    }
+                    onOnState = {
+                        println("on")
+                        state = true
+                    }
+                    getCurrent = { getState() }
+                }
+                textfield("text1") {
+                    text = "Textfield"
+                    update = {
+                        println("Received: $it")
+                    }
+                }
+            })
+    }
 
-        }
+    private fun getState(): Boolean {
+        return this.state
     }
 
     override fun getMqttSubscriptionsTopics(): ArrayList<Topic> {
